a5fd19d1e95c654b62b4d1777aa9687aeca36003
ChoiSeongHo-h <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-06-04 17:23:01 +0900

    Add files via upload

---
+++ 42._Trapping_Rain_Water.py
@@ -0,0 +1,42 @@
+"""
+★★★/★★★
+https://leetcode.com/problems/trapping-rain-water/
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        output = 0
+        if len(height) <= 2:
+            return output
+
+        i = 0
+        while i < len(height) - 2:
+            if height[i + 1] >= height[i]:
+                i = i + 1
+                continue
+
+            localMax = -1
+            localMaxIdx = -1
+            j = i + 2
+
+            # 1. larger than height[i] case 
+            while j < len(height):
+                if height[j] > localMax:
+                    localMax = height[j]
+                    localMaxIdx = j
+
+                    if height[j] >= height[i]:
+                        for k in range(i + 1, j):
+                            output = output + height[i] - height[k]
+                        i = j - 1
+                        break
+                j = j + 1
+
+                # 2. smaller than height[i] case
+                if j == len(height):
+                    for k in range(i + 1, localMaxIdx):
+                        if localMax > height[k]:
+                            output = output + localMax - height[k]
+                    i = localMaxIdx - 1
+            i = i + 1
+        return output
