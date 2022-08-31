b20448f74b0f9fc5fc11d400f96e889b6bbc0c9e
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-08-31 20:16:39 +0900

    Create 926_Flip_String_to_Monotone.cpp

---
+++ 926_Flip_String_to_Monotone.cpp
@@ -0,0 +1,40 @@
+/*
+https://leetcode.com/problems/flip-string-to-monotone-increasing/
+-> Accepted (Medium)
+*/
+
+class Solution
+{
+public:
+    int minFlipsMonoIncr(string s)
+    {
+        vector<int> l2rNum1(s.size()+1, 0);
+        vector<int> r2lNum0(s.size()+1, 0);
+
+        for(int i = 1; i<s.size()+1; i++)
+        {
+            l2rNum1[i] = l2rNum1[i-1];
+            if(s[i-1] == '1')
+            {
+                l2rNum1[i]++;
+            }
+        }
+        
+        for(int i = s.size()-1; i>=0; i--)
+        {
+            r2lNum0[i] = r2lNum0[i+1];
+            if(s[i] == '0')
+            {
+                r2lNum0[i]++;
+            }
+        }
+        int ans = 10e6;
+        for(int i = 0; i<s.size()+1; i++)
+        {
+            ans = min(ans, l2rNum1[i] + r2lNum0[i]);
+        }
+        
+        
+        return ans;
+    }
+};
