404a8e3291f55e29d223293714bbd74645d789b5
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-08-31 19:03:23 +0900

    Create 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.cpp

---
+++ 1647_Minimum_Deletions_to_Make_Character_Frequencies_Unique.cpp
@@ -0,0 +1,47 @@
+/*
+https://leetcode.com/problems/minimum-deletions-to-make-character-frequencies-unique/
+-> Accepted (Medium)
+*/
+
+
+bool cmp(int a, int b)
+{
+    return a>b;
+}
+
+class Solution
+{
+public:
+    
+    int minDeletions(string s)
+    {
+        vector<int> nums(26, 0);
+        for(int i = 0; i<s.size(); i++)
+        {
+            nums[s[i]-97]++;
+        }
+
+        sort(nums.begin(), nums.end(), cmp);
+        
+        int cnt = 0;
+        auto it0 = nums.begin();
+        auto it1 = nums.begin()+1;
+        while(it1 != nums.end() && !(*it0 == 0 && *it1 == 0))
+        {
+            if(*it0 <= *it1)
+            {
+                *it1 = *it1-1;
+                cnt++;
+                if(*it0 == 0 && *it1 == 0)
+                {
+                    it0++;
+                    it1++;
+                }
+                continue;
+            }
+            it0++;
+            it1++;
+        }
+        return cnt;
+    }
+};
