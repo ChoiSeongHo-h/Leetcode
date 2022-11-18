def877007a8930ddbb62c092a67fdbb209278ae7
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-11-18 14:53:04 +0900

    Create 00435_Non-overlapping_Intervals.cpp

---
+++ 00435_Non-overlapping_Intervals.cpp
@@ -0,0 +1,32 @@
+/*
+https://leetcode.com/problems/generate-parentheses/
+-> Accepted (Medium)
+*/
+
+class Solution 
+{
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) 
+    {
+        map<int, vector<int>> end_to_start_map;
+        for(auto& i : intervals)
+        {
+            end_to_start_map[i[1]-1].emplace_back(i[0]);
+        }
+
+        int start = -100000;
+        int ans = 0;
+        for(auto& i : end_to_start_map)
+        {
+            for(auto& j : i.second)
+            {
+                if(j > start)
+                    start = i.first;
+                else
+                    ++ans;
+            }
+        }
+
+        return ans;
+    }
+};
