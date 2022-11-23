03329f56fe54d43140223d4b7740d87ed306cc5b
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-11-23 22:04:11 +0900

    Create 00053_Maximum_Subarray.cpp

---
+++ 00053_Maximum_Subarray.cpp
@@ -0,0 +1,22 @@
+/*
+https://leetcode.com/problems/maximum-subarray/description/
+-> Accepted (Medium)
+*/
+
+class Solution 
+{
+public:
+    int maxSubArray(vector<int>& nums) 
+    {
+        int global_maxima = nums[0];
+        int local_maxima = nums[0];
+        for(int i = 1; i<nums.size(); ++i)
+        {
+            int candidate = nums[i];
+            local_maxima = max(candidate, local_maxima + nums[i]);
+            global_maxima = max(global_maxima, local_maxima);
+        }
+
+        return global_maxima;
+    }
+};
