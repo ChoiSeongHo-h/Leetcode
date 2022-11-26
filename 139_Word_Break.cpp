a764d83a3ce97c2e45b331aa8e152e071c523cbf
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-11-26 21:03:00 +0900

    Create 139_Word_Break.cpp

---
+++ 139_Word_Break.cpp
@@ -0,0 +1,27 @@
+/*
+https://leetcode.com/problems/word-break/
+-> Accepted (Medium)
+*/
+
+class Solution 
+{
+public:
+    bool wordBreak(string s, vector<string>& wordDict) 
+    {
+        vector<bool> possibles(s.length()+1, 0);
+        possibles[0] = true;
+        for(int idx = 0; idx < s.length(); ++idx)
+        {
+            for(auto& word : wordDict)
+            {
+                if(idx+1 < word.length())
+                    continue;
+                if(s.substr(idx+1-word.length(), word.length()).find(word) == string::npos)
+                    continue;
+
+                possibles[idx+1] = possibles[idx+1] + possibles[idx+1-word.length()];
+            }
+        }
+        return possibles.back();
+    }
+};
