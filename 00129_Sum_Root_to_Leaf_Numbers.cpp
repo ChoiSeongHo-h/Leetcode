2b739304f62db73f11928ef02df020661e5474d7
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-11-30 17:15:18 +0900

    Create 00129_Sum_Root_to_Leaf_Numbers.cpp

---
+++ 00129_Sum_Root_to_Leaf_Numbers.cpp
@@ -0,0 +1,44 @@
+/*
+https://leetcode.com/problems/generate-parentheses/
+-> Accepted (Medium)
+*/
+
+class Solution 
+{
+public:
+    int sumNumbers(TreeNode* root) 
+    {
+        uint32_t ans = 0;
+        stack<pair<TreeNode*, TreeNode*>> s;
+        vector<TreeNode*> path;
+        s.emplace(make_pair(root, nullptr));
+        while(!s.empty())
+        {
+            auto node = s.top();
+            s.pop();
+
+            while(node.second != nullptr && path.size() != 0 && path.back() != node.second)
+            {
+                path.pop_back();
+            }
+            path.emplace_back(node.first);
+
+            if(node.first->left != nullptr)
+                s.emplace(make_pair(node.first->left, node.first));
+            if(node.first->right != nullptr)
+                s.emplace(make_pair(node.first->right, node.first));
+            
+            if(node.first->left == nullptr && node.first->right == nullptr)
+            {
+                uint32_t padding = 1;
+                for(int i = path.size()-1; i>=0; --i)
+                {
+                    ans += padding*uint32_t(path[i]->val);
+                    padding *= 10;
+                }
+            }
+        }
+
+        return int(ans);
+    }
+};
