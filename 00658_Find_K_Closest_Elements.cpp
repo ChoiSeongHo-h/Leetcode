a16136a6ea6af14acfd24a32cfd0586d1ba33494
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-12-02 20:55:54 +0900

    Update 00658_Find_K_Closest_Elements.cpp

--- 00658_Find_K_Closest_Elements.cpp
+++ 00658_Find_K_Closest_Elements.cpp
@@ -8,27 +8,18 @@ class Solution
 public:
     vector<int> findClosestElements(vector<int>& arr, int k, int x) 
     {
-        int left = 0;
-        int right = arr.size()-1;
-        int mid = (left+right+1)/2;
-        int min_idx = mid;
-        while(left < right)
-        {
-            if(arr[mid] == x)
-                break;
-            
-            if(x-arr[mid] > 0)
-                left = mid+1;
-            if(x-arr[mid] < 0)
-                right = mid-1;
-            
-            mid = (left+right+1)/2;
-            //printf("test : %d %d\n", mid, arr[mid]);
-            if(min_idx < mid && abs(arr[min_idx] - x) > abs(arr[mid] - x))
-                min_idx = mid;
-            else if(min_idx > mid && abs(arr[min_idx] - x) >= abs(arr[mid] - x))
-                min_idx = mid;
-        }
+        auto it = lower_bound(arr.begin(), arr.end(), x);
+        int min_idx = -1;
+        if(it == arr.end())
+            min_idx = arr.size()-1;
+        else if(x < arr.front())
+            min_idx = 0;
+        else if(x == *it)
+            min_idx = distance(arr.begin(), it);
+        else if(abs(x-*prev(it)) <= abs(x-*it))
+            min_idx = distance(arr.begin(), prev(it));
+        else
+            min_idx = distance(arr.begin(), it);
         
         deque<int> deq;
         int l_idx = min_idx-1;
@@ -64,7 +55,7 @@ public:
         vector<int> ans;
         for(auto i : deq)
             ans.emplace_back(i);
-            
+
         return ans;
     }
 };
