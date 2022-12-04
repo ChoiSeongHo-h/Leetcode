7b4bf83cf9f7ad59c4b03a34175be6680e5f21d4
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2022-12-05 00:54:58 +0900

    Create 00718_Maximum_Length_of_Repeated_Subarray.cpp

---
+++ 00718_Maximum_Length_of_Repeated_Subarray.cpp
@@ -0,0 +1,29 @@
+"""
+https://leetcode.com/problems/maximum-length-of-repeated-subarray/
+-> Accepted (Medium)
+"""
+  
+class Solution 
+{
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) 
+    {
+        int m = nums2.size();
+        int n = nums1.size();
+        int ans = 0;
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(int i = 1; i<=m; ++i)
+        {
+            for(int j = 1; j<=n; ++j)
+            {
+                if(nums2[i-1] != nums1[j-1])
+                    continue;
+
+                dp[i][j] = dp[i-1][j-1]+1;
+                ans = max(ans, dp[i][j]);
+            }
+        }
+
+        return ans;
+    }
+};
