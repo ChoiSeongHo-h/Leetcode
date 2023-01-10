7aa257632874d37c8301c1ed138d4450ed17252f
ChoiSeongHo <72921481+ChoiSeongHo-h@users.noreply.github.com>
2023-01-11 05:05:14 +0900

    Update 00494_Target_Sum.cpp

--- 00494_Target_Sum.cpp
+++ 00494_Target_Sum.cpp
@@ -7,24 +7,27 @@ class Solution
 {
 public:
     vector<vector<int>> dp;
+    int tot;
 
     int search(vector<int>& nums, int target, int idx, int acc)
     {
         if(idx == nums.size())
             return acc == target;
         
-        int next0 = acc+nums[idx]+1000;
+        int next0 = acc+nums[idx] + tot;
         if(dp[idx][next0] == -9999)
-            dp[idx][next0] = search(nums, target, idx+1, next0-1000);
-        int next1 = acc-nums[idx]+1000;
+            dp[idx][next0] = search(nums, target, idx + 1, next0 - tot);
+        int next1 = acc - nums[idx] + tot;
         if(dp[idx][next1] == -9999)
-            dp[idx][next1] = search(nums, target, idx+1, next1-1000);
-        return dp[idx][next0]+dp[idx][next1];
+            dp[idx][next1] = search(nums, target, idx + 1, next1 - tot);
+        return dp[idx][next0] + dp[idx][next1];
     }
 
     int findTargetSumWays(vector<int>& nums, int target) 
     {
-        dp = vector<vector<int>>(nums.size(), vector<int>(2001, -9999));
-        return search(nums, target, 0, 0);
+        tot = abs(accumulate(nums.begin(), nums.end(), 0));
+        dp = vector<vector<int>>(nums.size(), vector<int>(2*tot+1, -9999));
+        int ans = search(nums, target, 0, 0);
+        return ans;
     }
 };
